ac85f4d29dad91e46ac60e9a5063a44c004247eb
Iris Yu <iris.yu0716@gmail.com>
2020-09-23 11:44:09 +0800

    add

---
+++ 1.js
@@ -0,0 +1,42 @@
+function decideAB() {
+
+    // 一一帶入
+    for (i = 0; i < 4; i++) {
+
+
+        // 判斷格式是否正確
+        if (不是數字) {
+
+            alert(i)
+        }
+
+        // 是數字就做判斷
+        else {
+
+            if (密碼是否有包含input數字) {
+
+                // 看位置是否一樣 
+                if (位置一樣) {
+
+                    把a加一
+                }
+
+                // 位置不一樣
+                else {
+
+                    把b加一
+                }
+            }
+
+            // 密碼沒有包含input數字
+            else {
+                // do nothing
+            }
+        }
+    }
+
+    // 顯示玩家input的數
+    // 顯示幾a幾b
+    // clear input
+
+}
